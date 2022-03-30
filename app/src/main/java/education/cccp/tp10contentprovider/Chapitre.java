d71fb438f4c7fdb43525d7074b6ccf99e2be38e8
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-30 09:37:20 +0200

    premier pas

---
+++ app/src/main/java/education/cccp/tp10contentprovider/Chapitre.java
@@ -0,0 +1,50 @@
+package education.cccp.tp10contentprovider;
+
+import static java.lang.String.format;
+
+import android.annotation.SuppressLint;
+
+public class Chapitre {
+    private int id;
+    private String name;
+    private String description;
+
+    public Chapitre(String name,
+                    String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public String toString() {
+        return format("Chapitre{id=%d, name='%s', description='%s'}",
+                id,
+                name,
+                description);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
