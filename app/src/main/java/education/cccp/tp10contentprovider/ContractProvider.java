e01101e348b0d1b82ffd02eaaea76c72a66bf5d0
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-30 09:55:19 +0200

    premier pas

---
+++ app/src/main/java/education/cccp/tp10contentprovider/ContractProvider.java
@@ -0,0 +1,10 @@
+package education.cccp.tp10contentprovider;
+
+public class ContractProvider {
+    public static final String DB_NAME = "chapitre.db";
+    public static final String TABLE_CHAPITRE = "table_chapitre";
+    public static final String COL_CHAPITRE_ID = "chapitre_id";
+    public static final String COL_CHAPITRE_NAME = "chapitre_name";
+    public static final String COL_CHAPITRE_DESCRIPTION = "chapitre_description";
+
+}
